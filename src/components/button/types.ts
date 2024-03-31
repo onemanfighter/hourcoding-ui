3d608265c8c1efab4977c594b7455d82d55a9f00
Amit Raikwar <managerarbusiness@gmail.com>
2024-03-31 23:01:17 +0530

    Added storybook for the demonstration of the component.

---
+++ src/components/button/types.ts
@@ -0,0 +1,10 @@
+type ButtonProps = {
+  variant: 'primary' | 'secondary';
+  label: string;
+  size: 'small' | 'medium' | 'large';
+  backgroundColor: string;
+  onClick: () => void;
+};
+
+export type { ButtonProps };
+// Path: src/components/button/Button.tsx
