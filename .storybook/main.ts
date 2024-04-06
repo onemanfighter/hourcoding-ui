a379e8fd663050806ff305acc6cc80d8857290cc
One Man Fighter <149316922+onemanfighter@users.noreply.github.com>
2024-04-06 23:33:34 +0530

    Moved stories to the parent level folder structure. (#6)

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,11 @@
 import type { StorybookConfig } from '@storybook/react-webpack5';
 
 const config: StorybookConfig = {
-  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  stories: [
+    '../src/**/*.mdx',
+    '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)',
+    '../examples/**/*.stories.@(js|jsx|mjs|ts|tsx)'
+  ],
   addons: [
     '@storybook/addon-webpack5-compiler-swc',
     '@storybook/addon-onboarding',
--- examples/Button.stories.ts
+++ examples/Button.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { Button } from '../components';
+import { Button } from '../src';
 
 const meta = {
   title: 'Example/Button',
