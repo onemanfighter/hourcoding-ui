d0c398d9cd158845d68dc5eb5f0f159383715f01
Amit Raikwar | OMF <149316922+onemanfighter@users.noreply.github.com>
2024-06-09 12:50:45 +0530

    fix(): Updating rollup (#45)

--- rollup.config.js
+++ rollup.config.js
@@ -1,36 +1,43 @@
-/* eslint-disable import/no-anonymous-default-export */
-import babel from '@rollup/plugin-babel';
+// rollup.config.js
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import peerDepsExternal from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
 import typescript from '@rollup/plugin-typescript';
+import dts from 'rollup-plugin-dts';
+import terser from '@rollup/plugin-terser';
+import postcss from 'rollup-plugin-postcss';
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: 'dist/index.js',
-      format: 'cjs',
-      sourcemap: true,
-    },
-    {
-      file: 'dist/index.esm.js',
-      format: 'esm',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    peerDepsExternal(),
-    postcss({
-      inject: true,
-    }),
-    typescript(),
-    babel({
-      extensions: ['.js', '.jsx', '.ts', '.tsx'],
-      exclude: 'node_modules/**',
-    }),
-    resolve(),
-    commonjs(),
-  ],
-};
+const packageJson = require('./package.json');
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: packageJson.main,
+        format: 'cjs',
+        sourcemap: true,
+      },
+      {
+        file: packageJson.module,
+        format: 'esm',
+        sourcemap: true,
+      },
+    ],
+    plugins: [
+      peerDepsExternal(),
+      resolve(),
+      commonjs(),
+      postcss(),
+      typescript({ tsconfig: './tsconfig.json' }),
+      terser(),
+    ],
+    external: ['react', 'react-dom', /\.css$/],
+  },
+  {
+    input: 'src/index.ts',
+    output: [{ file: 'dist/types.d.ts', format: 'esnext' }],
+    plugins: [dts.default()],
+  },
+];
